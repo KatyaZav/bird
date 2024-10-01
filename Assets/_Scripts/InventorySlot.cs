0dff268572c4fa81b438342b65b9b72c3069902a
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-10-01 19:12:48 +0500

    Add InventorySlot

---
+++ Assets/_Scripts/InventorySlot.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot
+{
+    private BaseItem _item;
+
+    public bool HasItem => _item != null;
+
+    public void SetItem(BaseItem item)
+    {
+        if (item != null)
+        {
+            Debug.LogError("Слот уже заполнен");
+            return;
+        }
+
+        _item = item;
+    } 
+
+    public BaseItem GetItem()
+    {
+        BaseItem currentItem = _item;
+        _item = null;
+        return currentItem; 
+    }
+}
