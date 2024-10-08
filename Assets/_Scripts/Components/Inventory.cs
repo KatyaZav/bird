32ab006dc5a1b0a31d7652940a1eab95a0f214d6
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-10-08 17:42:50 +0500

    refactor inventory

---
+++ Assets/_Scripts/Components/Inventory.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Inventory
+{
+    private ItemBase _item = null;
+    private Transform _itemPlace;
+
+    public bool IsEmpty => _item == null;
+    
+    public Inventory(Transform itemPlace)
+    {
+        _itemPlace = itemPlace;
+    }
+
+    public void SetItem(ItemBase item)
+    {
+        if (IsEmpty == false)
+        {
+            Debug.LogError("Слот уже заполнен");
+            return;
+        }
+
+        _item = item;
+
+        _item.transform.parent = _itemPlace;
+        _item.transform.position = Vector3.zero;
+    }
+
+    public ItemBase GetItem()
+    {
+        _item.transform.parent = null;
+
+        ItemBase currentItem = _item;
+        _item = null;
+        return currentItem;
+    }
+}
