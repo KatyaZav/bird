f1373e19927004a2da31a589e7beb1d25d3c5e3b
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-10-08 17:10:50 +0500

    refactor healItem

---
+++ Assets/_Scripts/HealItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealItem : ItemBase
+{
+    [SerializeField] private int _healthCount;
+
+    public override void Use(GameObject owner)
+    {
+        base.Use(owner);
+
+        Health health = owner.GetComponent<Health>();
+
+        health.Increse(_healthCount);
+    }
+}
